be0df0755c257551705f8d40f51f464cfcb60334
Aylin Bahar <aylin_65@proton.me>
2026-06-15 12:59:22 +0200

    file created, refactored all files, imports, loop implemented

---
+++ game_logic.py
@@ -0,0 +1,63 @@
+from ascii_art import *
+import random
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+def play_game():
+    secret_word = get_random_word()
+    print(secret_word)
+    guessed_letters = []
+    mistakes = 0
+
+    print("Welcome to Snowman Meltdown!")
+    # For now, display the initial game state.
+    display_game_state(mistakes, secret_word, guessed_letters)
+    while mistakes < len(STAGES) - 1:
+
+        display_game_state(mistakes, secret_word,guessed_letters)
+        guess = input("Guess a letter: ").lower()
+        if guess in secret_word:
+            if guess not in guessed_letters:
+                guessed_letters.append(guess)
+        else:
+            mistakes += 1
+            print("You guessed:", guess)
+            print(f"{guess} not in the secret word")
+        word_guessed = True
+
+        for letter in secret_word:
+            if letter not in guessed_letters:
+                word_guessed = False
+                break
+
+        if word_guessed:
+            print("You saved the snowman")
+            break
+    if mistakes >= len(STAGES) - 1:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        print("The snowman melted!")
+        print(f"The secret wird was : {secret_word} ")
+
+
+
+
+
